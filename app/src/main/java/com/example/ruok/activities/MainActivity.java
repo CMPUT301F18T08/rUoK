4287392fa5089904f8c7833e06e764bb3853d11c
Yuhan Ye <2232191103@qq.com>
2018-10-28 17:56:25 -0600

    sign in and sign up intent

--- app/src/main/java/com/example/ruok/activities/MainActivity.java
+++ app/src/main/java/com/example/ruok/activities/MainActivity.java
@@ -1,15 +1,36 @@
 package com.example.ruok.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import com.example.ruok.R;
 
-public class MainActivity extends AppCompatActivity {
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
+public class MainActivity extends AppCompatActivity {
+    public static final String SignInInfo = "DEFAULT";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void SignUp(View view) {
+        // Do something in response to button
+        Intent intent_SignUp = new Intent(this, SignUpActivity.class);
+        startActivity(intent_SignUp);
+    }
+
+    public void SignIn(View view){
+        Intent intent_SignIn = new Intent(this, SignUpActivity.class);
+        EditText userName_input = (EditText) findViewById(R.id.editText);
+        EditText password_input = (EditText) findViewById(R.id.editText);
+        String userName = userName_input.getText().toString();
+        String password = password_input.getText().toString();
+        intent_SignIn.putExtra(SignInInfo, userName+password);
+        startActivity(intent_SignIn);
     }
 }
