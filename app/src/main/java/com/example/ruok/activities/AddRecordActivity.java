f32ba316d4400811b3dbf912646105f2e497fafa
Yuhan Ye <2232191103@qq.com>
2018-10-28 18:05:04 -0600

    add record intent

--- app/src/main/java/com/example/ruok/activities/AddRecordActivity.java
+++ app/src/main/java/com/example/ruok/activities/AddRecordActivity.java
@@ -1,5 +1,6 @@
 package com.example.ruok.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,5 +12,6 @@ public class AddRecordActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_record);
+        Intent intent = getIntent();
     }
 }
