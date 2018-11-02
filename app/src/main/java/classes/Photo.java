4e3643a836e0c49498426bf7bb5e260b143df4cd
Yuanming Di <ydi1@ualberta.ca>
2018-11-02 02:57:23 -0600

    edit Photo class

--- app/src/main/java/classes/Photo.java
+++ app/src/main/java/classes/Photo.java
@@ -1,8 +1,18 @@
 package classes;
 
+import java.io.File;
+
 public abstract class Photo {
     private int height;
     private int width;
     private int resolution;
     private int size = 65536;
+    private File photoGraph;
+
+    public File getPhotoGraph() {
+        return photoGraph;
+    }
+    public void setPhotoGraph(File photoGraph) { this.photoGraph = photoGraph; }
+
+
 }
