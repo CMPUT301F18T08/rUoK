3586497f66f7a6c689cc7ff6c962bea02163b38a
Yuanming Di <ydi1@ualberta.ca>
2018-11-02 00:57:05 -0600

    add Problem functions to BodyLocation

--- app/src/main/java/classes/BodyLocation.java
+++ app/src/main/java/classes/BodyLocation.java
@@ -1,5 +1,21 @@
 package classes;
 
+import java.util.ArrayList;
+
 public abstract class BodyLocation {
+    private ArrayList<Problem> problems = new ArrayList<>();
+
+    // Problem func
+    public ArrayList<Problem> getProblems() {
+        return problems;
+    }
+
+    public void addProblem(Problem problem){
+        problems.add(problem);
+    }
+
+    public void deleteProblem(Problem problem){
+        problems.remove(problem);
+    }
 
 }
