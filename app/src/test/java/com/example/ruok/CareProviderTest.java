a307616ffdedbceb6b9dbf96b8c49939d413e27d
Boxiao <boxiao1@ualberta.ca>
2018-11-01 19:04:59 -0600

    caretest

--- app/src/test/java/com/example/ruok/CareProviderTest.java
+++ app/src/test/java/com/example/ruok/CareProviderTest.java
@@ -1,4 +1,29 @@
 package com.example.ruok;
+import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Date;
+
+import classes.Patient;
+import classes.Problem;
+import classes.Record;
+import static org.junit.Assert.*;
 public class CareProviderTest {
+	@Test
+    public void addPatientTest(){
+        Patient yasuo = new Patient();
+        yasuo.addProblem(problem);
+        assertEquals(problem,yasuo.getProblems().get(0));
+    }
+    @Test
+    public void addCommentTest(){
+
+        Patient yasuo = new Patient();
+        Problem problem = new Problem();
+        Record record = new Record("a","b",new Date(),"hi");
+        yasuo.addProblem(problem);
+        yasuo.addRecord(problem,record);
+        record.setComment("Hasagi");
+        assertEquals("Hasagi",record.getComment());
+    }
 }
