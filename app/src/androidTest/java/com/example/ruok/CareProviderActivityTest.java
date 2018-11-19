ca3d9ff52dcffce55ee51f45fadedeb1e3b79402
Yuhan Ye <2232191103@qq.com>
2018-11-19 14:20:09 -0700

    intent testing for careproviderActivity finished

--- app/src/androidTest/java/com/example/ruok/CareProviderActivityTest.java
+++ app/src/androidTest/java/com/example/ruok/CareProviderActivityTest.java
@@ -1,18 +1,23 @@
 package com.example.ruok;
 import android.app.Activity;
 
+import com.example.ruok.activities.AddPatientActivity;
 import com.example.ruok.activities.CareProviderActivity;
 import com.example.ruok.activities.ResetPasswordActivity;
 import com.example.ruok.activities.SearchActivity;
 import com.robotium.solo.Solo;
 
 import android.support.test.rule.ActivityTestRule;
+import android.test.ActivityInstrumentationTestCase2;
+
+import org.junit.Test;
+
 import classes.CareProvider;
 
 import static android.support.test.InstrumentationRegistry.getInstrumentation;
 
 /* created by Yuhan Ye*/
-public class CareProviderActivityTest extends ActivityTestRule{
+public class CareProviderActivityTest extends ActivityInstrumentationTestCase2 {
     private Solo solo;
     private CareProvider yyh = new CareProvider();
 
@@ -27,23 +32,25 @@ public class CareProviderActivityTest extends ActivityTestRule{
     public void testStart() throws Exception{
         Activity activity = getActivity();
     }
-
+    @Test
     public void testAddPatient() throws Exception{
         CareProviderActivity activity = (CareProviderActivity)solo.getCurrentActivity();
         solo.assertCurrentActivity("should be CareProviderActivity",CareProviderActivity.class);
-        solo.clickOnButton("ADD A PATIENT");
-//        solo.assertCurrentActivity();
+        solo.clickOnButton("add a patient");
+        solo.assertCurrentActivity("should be AddPatientActivity", AddPatientActivity.class);
     }
+    @Test
     public void testSearch() throws Exception{
         CareProviderActivity activity = (CareProviderActivity)solo.getCurrentActivity();
         solo.assertCurrentActivity("should be CareProviderActivity",CareProviderActivity.class);
-        solo.clickOnButton("SEARCH");
+        solo.clickOnButton("search");
         solo.assertCurrentActivity("should be SearchActivity", SearchActivity.class);
     }
+    @Test
     public void resetPassword() throws Exception{
         CareProviderActivity activity = (CareProviderActivity)solo.getCurrentActivity();
         solo.assertCurrentActivity("should be CareProviderActivity",CareProviderActivity.class);
-        solo.clickOnButton("RESET PASSWORD");
+        solo.clickOnButton("reset password");
         solo.assertCurrentActivity("should be ResetPasswordActivity", ResetPasswordActivity.class);
     }
 }
